16268f281d3f1603bbbf1fa0f11ecf91cf3d678d
islamghany <islamghany3@gmail.com>
2021-09-28 09:10:27 +0200

    connect to the mysql db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
+	"islamghany/snippetbox/pkg/models/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -10,23 +13,33 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
 
 	ADDR := os.Getenv("ADDR")
+	DNS := os.Getenv("DNS")
+
 	if len(ADDR) < 1 {
 		ADDR = ":4000"
 	}
 	addr := flag.String("addr", ADDR, "Http Network address")
+	dsn := flag.String("dns", DNS, "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -35,8 +48,19 @@ func main() {
 		Handler:  app.routes(),
 	}
 	infoLog.Println("Starting server on port", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	// will call os.Exit(1)
 	errorLog.Fatal(err)
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dns)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/creack/pty v1.1.15 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	golang.org/x/sys v0.0.0-20210923061019-b8560ed6a9b7 // indirect
 )
